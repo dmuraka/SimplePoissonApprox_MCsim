ec2d28fd6ad1a742dda81f59b1f8ca9a1f81c4ff
dmuraka <45805390+dmuraka@users.noreply.github.com>
2021-11-04 17:05:59 +0900

    Update PoissonApprox_MCsim_case2(spatial)_code.R
    modified version of the approximation (GAM)

--- PoissonApprox_MCsim_case2(spatial)_code.R
+++ PoissonApprox_MCsim_case2(spatial)_code.R
@@ -12,26 +12,26 @@
 # n   : Sample size
 
 dgp_sp<-function(b, dpar, n){
-
+  
   rqpois <- function(n, mu, dpar){
     rnbinom(n = n, mu = mu, size = mu/(dpar-1))
   }
-
+  
   coords  <- cbind(rnorm(n),rnorm(n))
   meig    <- meigen(coords)
   sf      <- meig$sf
   ev      <- meig$ev
   r       <- rnorm(ncol(sf),sd=sqrt(ev))
-
+  
   x	      <- cbind(1, rnorm(n), rnorm(n))
   xb      <- x %*% b
   sp_comp <- sd(xb)*scale(sf%*%r)
   y0      <- xb + sp_comp
   y       <- rqpois(n,mu=exp(y0),dpar=dpar)
-
+  
   x	      <- as.data.frame(x)
   names(x)<- paste("var",1:length( b ),sep="")
-
+  
   return(list(y=y, x=x, coords=coords, sp_comp=sp_comp))
 }
 
@@ -39,9 +39,10 @@ dgp_sp<-function(b, dpar, n){
 #######################################################################################
 #######################################################################################
 
+#install.packages("Matrix")
 #install.packages("mgcv")
 #install.packages("spmoran")
-library(mgcv);library(spmoran)
+library(mgcv);library(spmoran);library(Matrix)
 nsim  <- 1000       # Number of iteration
 b     <- c(NA,2,0.5)# True regression coefficients (NA for intercept b[0])
 dpar  <- 5          # True dispersion parameter
@@ -68,7 +69,7 @@ for(const in c(-2,-1,0,1,2)){# True value for the intercept (b[1])
     Bse3       <- matrix(0,nrow=nsim,ncol=4)
     Rmse_ranef0<- matrix(0,nrow=nsim,ncol=4)
     for(i in 1:nsim){
-
+      
       dum<-1
       ecount <-0
       while(dum==1){
@@ -78,22 +79,41 @@ for(const in c(-2,-1,0,1,2)){# True value for the intercept (b[1])
         y       <- dgp$y
         coords  <- dgp$coords
         sp_comp <- dgp$sp_comp
-
+        
         ######### Poisson regression
         test1 <-try(mod1   <- gam(method="ML", family="poisson",y~x[,2]+x[,3]+s(coords[,1],coords[,2],m=2,bs="gp")))
-
+        
         ######### Overdispersed Poisson regression
         test2 <-try(mod2   <- gam(method="ML", family="quasipoisson",y~x[,2]+x[,3]+s(coords[,1],coords[,2],m=2,bs="gp")))
-
+        
         ######### Proposed approximation #####################
         zrat  <- sum(y==0)/n
         y3    <- log(y+0.5) - (1+0.5*zrat)/(y+0.5)
         test3 <- try(mod3   <- gam(method="ML", weights=y+0.5, y3~x[,2]+x[,3]+s(coords[,1],coords[,2],m=2,bs="gp")))
+        sig3  <- mod3$sig2
+        nb    <- length(b)
+        
+        Xs    <- predict(mod3,type="lpmatrix")
+        lpred3<- Xs%*%coef(mod3)
+        pred3 <- exp(lpred3)
+        wXs   <- c(sqrt(pred3))*Xs
+        wy    <- c(sqrt(pred3))*(lpred3 + (y - pred3)/pred3)
+        
+        S_mat <-list(NULL)
+        S_mat[[1]]<-matrix(0,nb,nb)
+        for(iii in 1:length(mod3$sp)){
+          S_mat[[iii+1]]<-mod3$sp[iii]*mod3$smooth[[iii]]$S[[1]]
+        }
+        V3    <- bdiag(S_mat)
+        beta3 <- solve(t(wXs)%*%wXs+V3) %*% t(wXs)%*%wy
+        bse3  <- sqrt(sig3*diag(solve(t(wXs)%*%wXs+V3)))
+        pred3 <- as.vector(exp( Xs %*% beta3 ))
+        ranef3<- Xs[,-(1:3)] %*% beta3[-(1:3)]
 
         ######### Taylor approximation
         y4    <- log(y+c)- c/(y+c)
         test4<-try(mod4     <- gam(method="ML", weights=y+0.5, y4~x[,2]+x[,3]+s(coords[,1],coords[,2],m=2,bs="gp")))
-
+        
         test5<-(class(test1)[1]=="try-error")|(class(test2)[1]=="try-error")|(class(test3)[1]=="try-error")|(class(test4)[1]=="try-error")
         dum<-ifelse(test5,1,0)
         if(class(test5)[1]=="try-error"){
@@ -101,34 +121,34 @@ for(const in c(-2,-1,0,1,2)){# True value for the intercept (b[1])
           print(ecount)
         }
       }
-
+      
       ######################## Summarize coefficient estimates
-      B1[i,]<-c(coefficients(mod1)[1],coefficients(mod2)[1],coefficients(mod3)[1],coefficients(mod4)[1])
-      B2[i,]<-c(coefficients(mod1)[2],coefficients(mod2)[2],coefficients(mod3)[2],coefficients(mod4)[2])
-      B3[i,]<-c(coefficients(mod1)[3],coefficients(mod2)[3],coefficients(mod3)[3],coefficients(mod4)[3])
-
+      B1[i,]<-c(coefficients(mod1)[1],coefficients(mod2)[1],beta3[1],coefficients(mod4)[1])
+      B2[i,]<-c(coefficients(mod1)[2],coefficients(mod2)[2],beta3[2],coefficients(mod4)[2])
+      B3[i,]<-c(coefficients(mod1)[3],coefficients(mod2)[3],beta3[3],coefficients(mod4)[3])
+      
       ######################## Summarize coefficient standard errors
-      Bse1[i,]<-c(summary(mod1)$se[1],summary(mod2)$se[1],summary(mod3)$se[1],summary(mod4)$se[1])
-      Bse2[i,]<-c(summary(mod1)$se[2],summary(mod2)$se[2],summary(mod3)$se[2],summary(mod4)$se[2])
-      Bse3[i,]<-c(summary(mod1)$se[3],summary(mod2)$se[3],summary(mod3)$se[3],summary(mod4)$se[3])
-
+      Bse1[i,]<-c(summary(mod1)$se[1],summary(mod2)$se[1],bse3[1],summary(mod4)$se[1])
+      Bse2[i,]<-c(summary(mod1)$se[2],summary(mod2)$se[2],bse3[2],summary(mod4)$se[2])
+      Bse3[i,]<-c(summary(mod1)$se[3],summary(mod2)$se[3],bse3[3],summary(mod4)$se[3])
+      
       ######################## Summarize spatial random effects estimates
-      ranef <- cbind(predict(mod1,type="terms")[,3],predict(mod2,type="terms")[,3],predict(mod3,type="terms")[,3],predict(mod4,type="terms")[,3])
+      ranef <- cbind(predict(mod1,type="terms")[,3],predict(mod2,type="terms")[,3],ranef3,predict(mod4,type="terms")[,3])
       Rmse_ranef0[i,]<-sqrt(colMeans((ranef-c(sp_comp))^2))
     }
-
+    
     rmse1<-colSums((B1-b[1])^2/nsim)#### RMSE
     rmse2<-colSums((B2-b[2])^2/nsim)
     rmse3<-colSums((B3-b[3])^2/nsim)
-
+    
     bias1<-colSums((B1-b[1])/nsim)  #### Bias
     bias2<-colSums((B2-b[2])/nsim)
     bias3<-colSums((B3-b[3])/nsim)
-
+    
     mean1_bse<-colSums(Bse1/nsim)   #### Mean coefficient standard errors
     mean2_bse<-colSums(Bse2/nsim)
     mean3_bse<-colSums(Bse3/nsim)
-
+    
     ################################### Summarize error statistics
     Rmse1<-rbind(Rmse1,c(c,n,const,rmse1))
     Rmse2<-rbind(Rmse2,c(c,n,const,rmse2))
@@ -139,7 +159,7 @@ for(const in c(-2,-1,0,1,2)){# True value for the intercept (b[1])
     Mean1_bse<-rbind(Mean1_bse,c(c,n,const,mean1_bse))
     Mean2_bse<-rbind(Mean2_bse,c(c,n,const,mean2_bse))
     Mean3_bse<-rbind(Mean3_bse,c(c,n,const,mean3_bse))
-
+    
     Rmse_ranef<-rbind(Rmse_ranef,c(c,n,const,t(colMeans(Rmse_ranef0)) ))
     print(const)
   }
@@ -184,9 +204,4 @@ rmse_rr <-as.data.frame(rbind(cbind(Rmse_ranef[,1:3],b[2],b[3],Rmse_ranef[,-(1:3
 names(rmse_rr)<-c("x","n","b0","b1","b2","poisson","od_poisson","proposed","taylor")
 
 rmse_rr   # RMSE of the spatial random effects
-
-
-
-
-
 
